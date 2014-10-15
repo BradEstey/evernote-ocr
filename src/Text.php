d6a20c62a8dc4935fff3dab49dd22492063f9a1d
Brad Estey <brad.estey@gmail.com>
2014-10-15 17:33:12 -0400

    Add a little validation to Text class.

--- src/Text.php
+++ src/Text.php
@@ -2,6 +2,8 @@
 
 namespace Estey\EvernoteOCR;
 
+use InvalidArgumentException;
+
 /**
  * Text
  *
@@ -31,8 +33,37 @@ class Text
      */
     public function __construct($text, $confidence)
     {
+        $this->setText($text);
+        $this->setConfidence($confidence);
+    }
+
+    /**
+     * Set text.
+     * 
+     * @param string $text
+     * @return $this
+     */
+    public function setText($text)
+    {
         $this->text = $text;
+        return $this;
+    }
+
+    /**
+     * Set confidence score.
+     * 
+     * @param integer $confidence
+     * @return $this
+     */
+    public function setConfidence($confidence)
+    {
+        if (!is_int($confidence) or $confidence < 0 or $confidence > 100) {
+            throw new InvalidArgumentException(
+                'Confidence score must be an integer between 0 and 100.'
+            );
+        }
         $this->confidence = $confidence;
+        return $this;
     }
 
     /**
